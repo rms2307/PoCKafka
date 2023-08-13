08cded633cd1f791ed05aa45de812c2cfdd2c917
Robson de Moraes Silva <rms2307@outlook.com>
2023-08-12 21:44:08 -0300

    Implementando consumer básico

---
+++ ConsumerKafka/Program.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using Serilog;
+
+var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+logger.Information("Testando o consumo de mensagens com Kafka");
+
+if (args.Length != 2)
+{
+    logger.Error(
+        "Informe 2 parâmetros: " +
+        "no primeiro o IP/porta para testes com o Kafka, " +
+        "no segundo o Topic a ser utilizado no consumo das mensagens...");
+    return;
+}
+
+string bootstrapServers = args[0];
+string nomeTopic = args[1];
+
+logger.Information($"BootstrapServers = {bootstrapServers}");
+logger.Information($"Topic = {nomeTopic}");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = bootstrapServers,
+    GroupId = $"{nomeTopic}-group-0",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+CancellationTokenSource cts = new();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+try
+{
+    using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+    {
+        consumer.Subscribe(nomeTopic);
+
+        try
+        {
+            while (true)
+            {
+                var cr = consumer.Consume(cts.Token);
+                logger.Information(
+                    $"Mensagem lida: {cr.Message.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+            logger.Warning("Cancelada a execução do Consumer...");
+        }
+    }
+}
+catch (Exception ex)
+{
+    logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                 $"Mensagem: {ex.Message}");
+}
